f6a7ee37c7a56e66df857ff64acb5aff641396ef
NeroQue <dominikbratek11@gmail.com>
2025-03-04 21:06:02 +0100

    FEAT: Made the very basic first window

---
+++ window.py
@@ -0,0 +1,29 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.root = Tk()
+        self.root.title("My First GUI App")
+        self.canvas = Canvas(self.root, width=self.width, height=self.height)
+        self.canvas.pack()
+        self.running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.canvas.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+
+    def close(self):
+        self.running = False
+
+        
+        
+win = Window(800, 600)
+win.wait_for_close()
