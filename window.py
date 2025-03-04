4dbf3f9bbe4a8f1790edba65451a691dbf21b9e9
NeroQue <dominikbratek11@gmail.com>
2025-03-04 21:12:24 +0100

    FIX: Added main

--- window.py
+++ window.py
@@ -2,28 +2,22 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.root = Tk()
-        self.root.title("My First GUI App")
-        self.canvas = Canvas(self.root, width=self.width, height=self.height)
-        self.canvas.pack()
-        self.running = False
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-        self.root.update_idletasks()
-        self.canvas.update()
+        self.__root.update_idletasks()
+        self.__root.update()
 
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self.__running = True
+        while self.__running:
             self.redraw()
+        print("window closed...")
 
     def close(self):
-        self.running = False
-
-        
-        
-win = Window(800, 600)
-win.wait_for_close()
+        self.__running = False
