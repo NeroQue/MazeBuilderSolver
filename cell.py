333f301b59864a35a003d7061018cffaf876a2e2
NeroQue <dominikbratek11@gmail.com>
2025-03-06 23:19:27 +0100

    FEAT: Added a method to draw a line between two cells

--- cell.py
+++ cell.py
@@ -7,28 +7,47 @@ class Cell:
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self.x1 = None
-        self.x2 = None
-        self.y1 = None
-        self.y2 = None
-        self.win = _win
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = _win
     
     def draw(self, x1, y1, x2, y2):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
+        if self._win is None:
+            return
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
-            self.win.draw_line(line, "red")
+            self._win.draw_line(line, "red")
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
-            self.win.draw_line(line, "red")
+            self._win.draw_line(line, "red")
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
-            self.win.draw_line(line, "red")
+            self._win.draw_line(line, "red")
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
-            self.win.draw_line(line, "red")
+            self._win.draw_line(line, "red")
+
+    def draw_move(self, to_cell, undo=False):
+        half_length = abs(self._x2 - self._x1) // 2
+        x_center = half_length + self._x1
+        y_center = half_length + self._y1
+
+        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = half_length2 + to_cell._x1
+        y_center2 = half_length2 + to_cell._y1
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self._win.draw_line(line, fill_color)
+            
 
 
