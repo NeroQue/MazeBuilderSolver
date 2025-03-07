745bc49dbbd907c6800fabf5be5b51352909851c
NeroQue <dominikbratek11@gmail.com>
2025-03-07 17:36:46 +0100

    FEAT: Implement Maze generation

--- cell.py
+++ cell.py
@@ -2,7 +2,7 @@ from window import Window
 from pointAndLine import Point, Line
 
 class Cell:
-    def __init__(self, _win):
+    def __init__(self, win = None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -11,7 +11,7 @@ class Cell:
         self._x2 = None
         self._y1 = None
         self._y2 = None
-        self._win = _win
+        self._win = win
     
     def draw(self, x1, y1, x2, y2):
         if self._win is None:
@@ -23,15 +23,27 @@ class Cell:
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line, "red")
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, "white")
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line, "red")
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "white")
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line, "red")
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "white")
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line, "red")
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "white")
 
     def draw_move(self, to_cell, undo=False):
         half_length = abs(self._x2 - self._x1) // 2
